5f9872eff91beccc4052b319812c5ddfc7541424
Michael Barrow <163350391+Portmanteaux26@users.noreply.github.com>
2025-09-23 16:50:39 -0500

    Update test_integration.py
    Fix import path

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,7 +1,7 @@
 import sys, os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src")))
-from src.main import run_simulation
+from main import run_simulation
 
 
 class TestIntegration:
